af2544a4c011d31fc1f58837759584d829ec6b4c
izadoramatias <contato.izadoramatias@gmail.com>
2021-10-29 01:13:21 -0300

    finish media queries

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,16 @@ VanillaTilt.init(document.querySelector(".container .content"), {
   glare: true,
   "max-glare": 0.5
 });
+
+function getApiEficazInfos(){
+  const url = `https://estagio.eficazmarketing.com/api/user`;
+
+  fetch(url)
+  .then(response => response.json()).then(data => {
+    console.log(data[0])
+  });
+  
+  
+}
+
+getApiEficazInfos()
