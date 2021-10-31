97301e8b867e0674b62269dd9e2ec2a21aa2397a
izadoramatias <contato.izadoramatias@gmail.com>
2021-10-31 19:00:46 -0300

    finish application

---
+++ scripts/home.js
@@ -0,0 +1,6 @@
+VanillaTilt.init(document.querySelector('.container .content'), {
+  max: 6,
+  speed: 250,
+  glare: true,
+  'max-glare': 0.5
+})
--- scripts/script.js
+++ scripts/script.js
@@ -1,75 +1,48 @@
-VanillaTilt.init(document.querySelector(".container .content"), {
-  max: 6,
-  speed: 250,
-  glare: true,
-  "max-glare": 0.5
-});
-
-
 // post request
 
-// function doPost(url, body){
-//   console.log(body)
+function submitDataToAPI(event) {
+  // event.preventDefault()
 
-//   let request = new XMLHttpRequest();
-//   request.open('POST', url, true);
-//   request.setRequestHeader("Content-type", "application/json");
-//   request.send(JSON.stringify(body));
+  let emailPessoal = document.getElementById('email-pessoal').value
+  let email = document.getElementById('email').value
+  let rua = document.getElementById('rua').value
+  let complemento = document.getElementById('complemento').value
+  let cep = document.getElementById('cep').value
+  let uf = document.getElementById('uf').value
+  let nomeCompleto = document.getElementById('name').value
+  let telefone = document.getElementById('telefone').value
+  let numero = document.getElementById('numero').value
+  let bairro = document.getElementById('bairro').value
+  let cidade = document.getElementById('cidade').value
 
-//   request.onload = () => {
-//     console.log(this.responseText);
-//   }
-  
-//   return request.responseText
+  const dataBody = {
+    seu_email: emailPessoal,
+    nome: nomeCompleto,
+    email: email,
+    telefone: telefone,
+    rua: rua,
+    numero: numero,
+    complemento: complemento,
+    bairro: bairro,
+    cidade: cidade,
+    uf: uf,
+    cep: cep
+  }
+  const url = "https://estagio.eficazmarketing.com/api/user"
+  console.log(dataBody)
 
-// }
-
-async function doPost(url, body) {
-  const response = await fetch(url, {
+  fetch(url, {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    method: 'POST',
-    body: JSON.stringify(body)
+    body: JSON.stringify(dataBody)
   })
 
-  return response;
-  console.log(response)
+  const takeModal = document.querySelector(".modal");
+  takeModal.style.visibility = "visible";
 }
 
+const takeFormToSubmit = document.querySelector('#formulario');
+takeFormToSubmit.addEventListener('submit', submitDataToAPI);
 
-
-function registerUser(){
-  preventDefault();
-  url = "http://127.0.0.1:5500/cadastro.html"
-
-  let emailPessoal = document.getElementById('email-pessoal').value;
-  let email = document.getElementById('email').value;
-  let rua = document.getElementById('rua').value;
-  let complemento = document.getElementById('complemento').value;
-  let cep = document.getElementById('cep').value;
-  let uf = document.getElementById('uf').value;
-  let nomeCompleto = document.getElementById('name').value;
-  let telefone = document.getElementById('telefone').value;
-  let numero = document.getElementById('numero').value;
-  let bairro = document.getElementById('bairro').value;
-  let cidade = document.getElementById('cidade').value;
-
-  body = {
-    "seu_email": emailPessoal,
-    "nome": nomeCompleto,
-    "email": email,
-    "telefone": telefone,
-    "rua": rua,
-    "número": numero,
-    "complemento": complemento,
-    "bairro": bairro,
-    "cidade": cidade,
-    "uf": uf, 
-    "cep": cep
-  }
-
-  console.log(email)
-  doPost(url, body);
-
-};
